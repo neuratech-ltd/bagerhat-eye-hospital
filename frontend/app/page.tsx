eb271eb61d19fb6f314eaaae4114e9888616139b
shajeebHaider <alihaiderShajeeb06@gmail.com>
2026-07-22 17:04:45 +0600

    some minor changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,13 +6,11 @@ import ServicesSection from "@/components/Home/ServicesSection";
 import BannnerFreeCamp from "@/components/Home/BannnerFreeCamp";
 import DoctorsList from "@/components/Home/DoctorsList";
 import Testimonials from "@/components/Home/Testimonials";
-import HomeScrollHandler from "@/components/layout/HomeScrollHandler";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center bg-zinc-50 font-sans dark:bg-black">
       <FadeInObserver />
-      <HomeScrollHandler />
       <HeroSection />
       <Marquee />
       <MissionSection />
